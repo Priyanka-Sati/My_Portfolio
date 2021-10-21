7aa3ee6fdb302848238fbd21bc2d18ad870cb788
Priyanka_sati <priyankasati1718@gmail.com>
2021-10-21 22:43:08 +0530

    Updates

---
+++ assets/skillsData.js
@@ -0,0 +1,31 @@
+
+export const skillList = [
+    {
+        skillName: 'HTML',
+        skillUrl: './assets/html.png',
+    },
+    {
+        skillName: 'CSS',
+        skillUrl: './assets/css.png',
+    },
+    {
+        skillName: 'JavaScript',
+        skillUrl: './assets/js.png',
+    },
+    {
+        skillName: 'React',
+        skillUrl: './assets/react.png',
+    },
+    {
+        skillName: 'Java',
+        skillUrl: './assets/java.png',
+    },
+    {
+        skillName: 'C++',
+        skillUrl: './assets/c++.png',
+    },
+    {
+        skillName: 'MySql',
+        skillUrl: './assets/mysql.png',
+    },
+];
