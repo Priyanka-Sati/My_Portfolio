7ca0be2cbcd9233da48dd4fdb69697d1ba31cacc
Priyanka Sati <69068748+Priyanka-Sati@users.noreply.github.com>
2022-02-15 07:06:41 +0530

    Update projectsData.js

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -4,7 +4,7 @@ export const projectsData = [
         projectName: 'Resume Builder',
         projectDescription:
             'This project allows user to easily maintain and build their resume. I build this project using React, JavaScript, CSS, HTML. From this project I learn how to implement Redux, hooks, and routing to different pages. I have use firebase for authentications and storing user information.',
-        imageUrl: './assets/resumebuilder.phg',
+        imageUrl: './assets/resumebuilder.png',
         videoUrl:
             'https://github.com/Priyanka-Sati/Resume_Builder',
         projectUrl: 'https://resume-builder-5bc10.web.app/',
